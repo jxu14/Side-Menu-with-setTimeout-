77863bafab9945500dc9f31ec6133ed69d941657
jxu14 <jeremyxu123@gmail.com>
2017-12-04 11:22:29 -0800

    idk

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-	console.log("sfsdhi");
+  let btn = $('button');
+  let content = $('#openbar');
+
+  let a = $("#countdown");
+  let first = $('#first');
+  let second = $('#second');
+
+
+  btn.append(content);
+  // btn.addEventListener("click", function() {
+  //   content.classList.toggle("show");
+  // })
+
+  btn.on("mouseover", function() {
+    // content.classList.toggle("show");
+    // content.style.width = "250px";
+    content.width("250px");
+  })
+
+  btn.on("mouseout", function() {
+    // content.classList.remove("show");
+    // console.log(content.classList);
+    // content.style.width = "0";
+    content.width("0");
+  })
+
+  first.on("mouseover", function() {
+  	let num = 999;
+  	for(let i = 0; i < num ; i++) {
+  	  setTimeout(function(){a.html(num - i)}, i*1000)
+  	}
+  })
+
 })
